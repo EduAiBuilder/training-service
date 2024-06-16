22c56e653fcbe167f3a6a03c10b14d18a52df7f4
Achiya Haviv <ahiya.haviv@bewith.io>
2024-06-16 18:00:15 +0300

    save to images sub dir

--- app/models_trainer/models_trainer.py
+++ app/models_trainer/models_trainer.py
@@ -1,20 +1,20 @@
 from app.models_trainer.trainer_client import fetch_trainer_data
 from app.models_trainer.classes import Trainer_images_by_category_response
-from fastai.vision.all import Path, L, download_images
+from fastai.vision.all import Path, download_images
 
 async def train_model(trainer_id):
     trainer_images: list[Trainer_images_by_category_response] = await fetch_trainer_data(trainer_id)
-    download_images_f(trainer_id, trainer_images)
+    save_images_by_category(trainer_id, trainer_images)
 
 
-def download_images_f(trainer_id: str, trainer_images: list[Trainer_images_by_category_response]):
-    path = Path(f'app/models_trainer/{trainer_id}')
+def save_images_by_category(trainer_id: str, trainer_images: list[Trainer_images_by_category_response]):
+    path = Path(f'app/models_trainer/images/{trainer_id}')
 
     if not path.exists():
         path.mkdir()
     for image in trainer_images:
         category = image.get('category')
-        urls=image.get('imageUrls')
+        urls= image.get('imageUrls')
         dest = (path/category)
         dest.mkdir(exist_ok=True)
-        download_images(dest, urls=L(urls))
+        download_images(dest, urls=urls)
