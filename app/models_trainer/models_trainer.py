9822f319a97c096827832f7bf156cf5cb6d5a01a
Achiya Haviv <ahiya.haviv@bewith.io>
2024-06-16 18:17:01 +0300

    base train flow

--- app/models_trainer/models_trainer.py
+++ app/models_trainer/models_trainer.py
@@ -1,10 +1,13 @@
 from app.models_trainer.trainer_client import fetch_trainer_data
 from app.models_trainer.classes import Trainer_images_by_category_response
-from fastai.vision.all import Path, download_images
+from fastai.vision.all import Path, download_images, get_image_files, verify_images, DataBlock, ImageBlock, CategoryBlock, parent_label, RandomSplitter, RandomResizedCrop, aug_transforms, vision_learner, resnet18, error_rate
 
 async def train_model(trainer_id):
     trainer_images: list[Trainer_images_by_category_response] = await fetch_trainer_data(trainer_id)
-    save_images_by_category(trainer_id, trainer_images)
+    path = save_images_by_category(trainer_id, trainer_images)
+    dls = set_data_loaders(path)
+    model = train_model(dls)
+
 
 
 def save_images_by_category(trainer_id: str, trainer_images: list[Trainer_images_by_category_response]):
@@ -18,3 +21,25 @@ def save_images_by_category(trainer_id: str, trainer_images: list[Trainer_images
         dest = (path/category)
         dest.mkdir(exist_ok=True)
         download_images(dest, urls=urls)
+    return path
+
+def set_data_loaders(path):
+    fns = get_image_files(path)
+    failed = verify_images(fns)
+    failed.map(Path.unlink);
+    animals = DataBlock(
+        blocks=(ImageBlock, CategoryBlock),
+        get_items=get_image_files,
+        splitter=RandomSplitter(valid_pct=0.2, seed=42),
+        get_y=parent_label,
+        item_tfms=RandomResizedCrop(224, min_scale=0.5),
+        batch_tfms=aug_transforms(),
+    )
+    return animals.dataloaders(path)
+
+def train_model(dls):
+    learn = vision_learner(dls, resnet18, metrics=error_rate)
+    learn.fine_tune(4)
+    return learn.export()
+
+
