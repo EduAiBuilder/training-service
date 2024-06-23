01649a24872375f72410a78e55440916017009ca
Achiya Haviv <ahiya.haviv@bewith.io>
2024-06-23 09:34:49 +0300

    upload models to s3

--- app/models_trainer/models_trainer.py
+++ app/models_trainer/models_trainer.py
@@ -1,20 +1,21 @@
+from .s3 import upload_to_s3
 from .trainer_client import fetch_trainer_data
 from .classes import Trainer_images_by_category_response
 from .trainer_callback import ProgressCallback
 from fastbook import *
 from fastai.vision.all import *
+import io
 
-async def main_training_process(trainer_id):
+async def main_training_process(trainer_id: int):
     trainer_images: list[Trainer_images_by_category_response] = await fetch_trainer_data(trainer_id)
     path = save_images_by_category(trainer_id, trainer_images)
     dls = set_data_loaders(path)
-    model = train_and_export_model(dls)
+    export_path = f'app/models_trainer/export/{trainer_id}.pkl'
+    train_and_export_model(dls,export_path)
+    upload_to_s3(trainer_id,export_path)
 
 
 def save_images_by_category(trainer_id: str, trainer_images: list[Trainer_images_by_category_response]):
-    if trainer_id == 6:
-        return Path(f'app/models_trainer/images/{trainer_id}')
-    
     path = Path(f'app/models_trainer/images/{trainer_id}')
 
     if not path.exists():
@@ -31,21 +32,20 @@ def set_data_loaders(path):
     fns = get_image_files(path)
     failed = verify_images(fns)
     failed.map(Path.unlink)  # Remove failed images
-    animals = DataBlock(
+    data_block = DataBlock(
         blocks=(ImageBlock, CategoryBlock),
         get_items=get_image_files,
         splitter=RandomSplitter(valid_pct=0.2, seed=42),
         get_y=parent_label,
         item_tfms=RandomResizedCrop(224, min_scale=0.5),
     )
-    dls = animals.dataloaders(path)
+    dls = data_block.dataloaders(path)
     print(f"Number of classes: {dls}")  # Check number of classes
     return dls
 
-def train_and_export_model(dls):
-    learn = vision_learner(dls, resnet18, metrics=error_rate, cbs=[ProgressCallback('http://localhost:8000/trainer/progress')])
+def train_and_export_model(dls,export_path:str):
+    learn = vision_learner(dls, resnet18, metrics=error_rate)
     learn.fine_tune(4)
-    learn.remove_cbs(ProgressCallback)
-    learn.export()  
-    return learn
+    learn.export(export_path)
+    return
 
---
+++ app/models_trainer/s3.py
@@ -0,0 +1,27 @@
+import boto3
+import os
+from app.config import settings
+
+
+def upload_to_s3(trainer_id:int, export_path: str):
+    s3_client = boto3.client('s3',
+                             aws_access_key_id=settings.aws_access_key_id,
+        aws_secret_access_key=settings.aws_secret_access_key,
+        region_name=settings.aws_region_name
+                             )
+    s3_object_key = f'models/{trainer_id}.pkl'
+
+    bucket = 'edu-ai-builder'
+    try:
+        # Open the file at export_path in binary read mode
+        with open(export_path, 'rb') as file_obj:
+            # Upload the file to S3 using the file object
+            s3_client.upload_fileobj(file_obj, bucket, s3_object_key)
+        
+        print(f'Model successfully uploaded to s3://{bucket}/{s3_object_key}')
+
+        os.remove(export_path)
+        print(f'Local file {export_path} has been removed successfully.')
+    except Exception as e:
+        print(f'Error uploading model to S3: {e}')
+    return s3_object_key
